57131620e5f91d50acb4a54c2000256d7451b263
Shoaib Dawood <shoaib.dawood1999@gmail.com>
2024-07-25 23:23:27 +0500

    Qestion # 2 list

---
+++ assigenent_5/question1.dart
@@ -0,0 +1,14 @@
+void main() {
+  List<String> names = [
+    "Ali",
+    "hamza",
+    "muanzah",
+    "shoaib",
+    "Kamar",
+    "Sana",
+    "saba",
+    "Dua",
+    "Shazaib"
+  ];
+  print(names);
+}
---
+++ assigenent_5/question2.dart
@@ -0,0 +1,10 @@
+void main() {
+  List<String> stud_Details = [
+    'Name: shoaib dawood',
+    'Class: 10',
+    'Roll No: 12723',
+    'Grade: B',
+    'Percentage: 81%'
+  ];
+  print(stud_Details);
+}
